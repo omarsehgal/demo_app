a4af2770e852ae6dd00d04338c0e9a98249f6a2f
omar sehgal <omar.sehgal@gmail.com>
2011-04-23 01:46:45 -0700

    removed the index html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 DemoApp::Application.routes.draw do
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation:
@@ -52,7 +51,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
